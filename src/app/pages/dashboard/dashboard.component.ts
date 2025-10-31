cef4212eebb8200809be9e2410b5a3e5af195303
AlexisADpm <alexisperez.1120@gmail.com>
2025-10-31 17:57:17 -0300

    Minimal changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,14 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { SummaryCardsComponent } from './components/summary-cards/summary-cards.component';
 import { ProjectsService } from '../../services/projects.service';
 import { NewsService } from '../../services/news.service';
+import { Charts } from './components/charts/charts';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
-  imports: [SummaryCardsComponent],
+  imports: [SummaryCardsComponent, Charts],
 })
 export class DashboardComponent implements OnInit {
-
   username: string | null = '';
   ngOnInit() {
     this.username = localStorage.getItem('username');
